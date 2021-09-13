d873fe77dd5ca5f2eee9646c9b36b6ff57d02452
Morebot <info@morebec.com>
2021-09-13 20:09:44 +0000

    [Framework] Improve HttpLoggerListener

--- src/Api/HttpLoggerListener.php
+++ src/Api/HttpLoggerListener.php
@@ -3,22 +3,24 @@
 namespace Morebec\Orkestra\Framework\Api;
 
 use Morebec\Orkestra\DateTime\ClockInterface;
-use Morebec\Orkestra\DateTime\DateTime;
 use Psr\Log\LoggerInterface;
-use Psr\Log\LogLevel;
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class HttpLoggerListener implements EventSubscriberInterface
 {
-    public const REQUEST_ID_KEY = 'RequestId';
+    public const REQUEST_ID_ATTRIBUTE = 'requestId';
+    private const REQUEST_STARTED_AT_ATTRIBUTE = 'requestStartedAt';
+    private const REQUEST_DURATION_ATTRIBUTE = 'requestDuration';
+
     private LoggerInterface $logger;
     private ClockInterface $clock;
 
-    public function __construct(LoggerInterface $logger, ClockInterface $clock)
+    public function __construct(ClockInterface $clock, LoggerInterface $logger)
     {
         $this->logger = $logger;
         $this->clock = $clock;
@@ -27,77 +29,26 @@ class HttpLoggerListener implements EventSubscriberInterface
     public static function getSubscribedEvents(): array
     {
         return [
-            KernelEvents::REQUEST => ['onRequest', 1],
-            KernelEvents::RESPONSE => ['onResponse', 256],
-            KernelEvents::EXCEPTION => ['onException', 256],
+            KernelEvents::REQUEST => ['onRequest', 1000],
+            KernelEvents::RESPONSE => ['onResponse', -1000],
+            KernelEvents::TERMINATE => ['onTerminate', -1000],
         ];
-    }
-
-    public function onException(ExceptionEvent $event): void
-    {
-        if (!$event->isMasterRequest()) {
-            return;
-        }
-
-        $request = $event->getRequest();
-        $throwable = $event->getThrowable();
-
-        /** @var DateTime $requestDate */
-        $requestDate = $request->attributes->get('Date', $this->clock->now());
-        $method = $request->getMethod();
-        $path = $request->getPathInfo();
-
-        $exceptionClass = \get_class($throwable);
-        $exceptionMessage = $throwable->getMessage();
-
-        $requestId = $request->attributes->get(self::REQUEST_ID_KEY);
-
-        $context = [
-            'path' => $path,
-            'method' => $method,
-            'hostname' => $request->getHost(),
-            'ipAddress' => $request->getClientIp(),
-            'date' => $requestDate,
-            'duration' => (int) (($this->clock->now()->getMillisTimestamp() * 1000) - ($requestDate->getMillisTimestamp() * 1000)),
-            'requestId' => $requestId,
-            'headers' => $request->headers->all(),
-//            'request' => (string) $request,
-//            'response' => (string) $response,
-            'exceptionClass' => $exceptionClass,
-            'exceptionMessage' => $exceptionMessage,
-            'exceptionFile' => $throwable->getFile(),
-            'exceptionLine' => $throwable->getLine(),
-            'exceptionTrace' => $throwable->getTrace(),
-        ];
-
-        $this->logger->error("$requestId - Exception encountered - {$method} {$path} - $exceptionClass - $exceptionMessage", $context);
     }
 
     public function onRequest(RequestEvent $event): void
     {
         $request = $event->getRequest();
+        $request->attributes->set(self::REQUEST_ID_ATTRIBUTE, $request->attributes->get(self::REQUEST_ID_ATTRIBUTE, Uuid::uuid4()->toString()));
 
-        $requestId = uniqid('req_', true);
-        $requestDate = $this->clock->now();
+        $request->attributes->set(self::REQUEST_STARTED_AT_ATTRIBUTE, $this->clock->now()->getMillisTimestamp() * 1000);
 
-        $request->attributes->set(self::REQUEST_ID_KEY, $requestId);
-        $request->attributes->set('Date', $requestDate);
-
-        $path = $request->getPathInfo();
-        $method = $request->getMethod();
-
-        $context = [
-            'path' => $path,
-            'method' => $method,
-            'hostname' => $request->getHost(),
-            'ipAddress' => $request->getClientIp(),
-            'date' => $requestDate,
-            'requestId' => $requestId,
-            'headers' => $request->headers->all(),
-//            'request' => (string) $request,
-//            'response' => (string) $response,
-        ];
-        $this->logger->info("$requestId - Request received - {$method} {$path}", $context);
+        $this->logger->info('Request received: {requestMethod} {requestPath} {requestScheme}', [
+            'requestId' => $request->attributes->get(self::REQUEST_ID_ATTRIBUTE),
+            'requestMethod' => $request->getMethod(),
+            'requestPath' => $request->getPathInfo(),
+            'requestScheme' => $request->getScheme(),
+            'requestUri' => $request->getUri(),
+        ]);
     }
 
     public function onResponse(ResponseEvent $event): void
@@ -105,36 +56,39 @@ class HttpLoggerListener implements EventSubscriberInterface
         $request = $event->getRequest();
         $response = $event->getResponse();
 
-        /** @var DateTime $requestDate */
-        $requestDate = $request->attributes->get('Date', $this->clock->now());
-        $method = $request->getMethod();
-        $statusCode = $response->getStatusCode();
-        $path = $request->getPathInfo();
+        $requestStartedAt = $request->attributes->get(self::REQUEST_STARTED_AT_ATTRIBUTE);
+        $requestDuration = ($requestStartedAt / 1000) - $this->clock->now()->getMillisTimestamp();
 
-        $requestId = $request->attributes->get(self::REQUEST_ID_KEY);
+        $request->attributes->set(self::REQUEST_DURATION_ATTRIBUTE, $requestDuration);
 
-        $context = [
-            'path' => $path,
-            'method' => $method,
-            'statusCode' => $statusCode,
-            'hostname' => $request->getHost(),
-            'ipAddress' => $request->getClientIp(),
-            'date' => $requestDate,
-            'duration' => (int) (($this->clock->now()->getMillisTimestamp() * 1000) - ($requestDate->getMillisTimestamp() * 1000)),
-            'requestId' => $requestId,
-            'headers' => $request->headers->all(),
-//            'request' => (string) $request,
-//            'response' => (string) $response,
-        ];
+        $this->logger->info('Response returned: {requestMethod} {requestPath} {requestScheme} {responseStatusCode} {requestDuration}', [
+            'requestId' => $request->attributes->get(self::REQUEST_ID_ATTRIBUTE),
+            'requestMethod' => $request->getMethod(),
+            'requestPath' => $request->getPathInfo(),
+            'requestScheme' => $request->getScheme(),
+            'requestUri' => $request->getUri(),
+            'responseStatusCode' => $response->getStatusCode(),
+            'requestDuration' => $requestDuration,
+        ]);
+    }
 
-        if (!$response->isSuccessful() && $response->headers->get('Content-Type') === 'application/json') {
-            $context['content'] = $response->getContent();
+    public function onTerminate(TerminateEvent $event): void
+    {
+        if (!$event->isMasterRequest()) {
+            return;
         }
 
-        $this->logger->log(
-            $response->isSuccessful() ? LogLevel::INFO : LogLevel::ERROR,
-            "$requestId - Response returned - {$statusCode} {$method} {$path}",
-            $context
-        );
+        $request = $event->getRequest();
+        $response = $event->getResponse();
+
+        $this->logger->info('{requestMethod} {requestPath} {requestScheme} {responseStatusCode}', [
+            'requestId' => $request->attributes->get(self::REQUEST_ID_ATTRIBUTE),
+            'requestMethod' => $request->getMethod(),
+            'requestPath' => $request->getPathInfo(),
+            'requestScheme' => $request->getScheme(),
+            'requestUri' => $request->getUri(),
+            'responseStatusCode' => $response->getStatusCode(),
+            'requestDuration' => $request->attributes->get(self::REQUEST_DURATION_ATTRIBUTE),
+        ]);
     }
 }
