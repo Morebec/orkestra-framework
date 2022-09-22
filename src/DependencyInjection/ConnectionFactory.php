dbbbd154a7a87d5ae59c15f3623f301575899f05
Morebot <info@morebec.com>
2022-09-22 17:25:45 +0000

    [Framework] Remove auto connect from connection factory

--- src/DependencyInjection/ConnectionFactory.php
+++ src/DependencyInjection/ConnectionFactory.php
@@ -23,20 +23,12 @@ class ConnectionFactory
     private LoggerInterface $logger;
 
     /**
-     * @param string             $connectionString database connection string
-     * @param Configuration|null $configuration    doctrine Connection configuration
-     * @param bool               $autoConnect      indicates if connecting to the database should be performed automatically when creating the connection
+     * @param string $connectionString database connection string
+     * @param Configuration|null $configuration doctrine Connection configuration
      */
-    public function __construct(
-        string $connectionString,
-        ?Configuration $configuration = null,
-        bool $autoConnect = true,
-        ?LoggerInterface $logger = null
-    ) {
+    public function __construct(string $connectionString, ?Configuration $configuration = null) {
         $this->configuration = $configuration ?? new Configuration();
         $this->connectionString = $connectionString;
-        $this->autoConnect = $autoConnect;
-        $this->logger = $logger ?? new NullLogger();
     }
 
     /**
@@ -47,36 +39,8 @@ class ConnectionFactory
      */
     public function create(): Connection
     {
-        $connection = DriverManager::getConnection([
+        return DriverManager::getConnection([
             'url' => $this->connectionString,
         ], $this->configuration);
-
-
-        if ($this->autoConnect) {
-            $retry = RetryStrategy::create()
-                ->maximumAttempts(50)
-                ->useExponentialBackoff(
-                    1000 * 10, // 10 seconds
-                    2.0,
-                    1000 * 60 * 5 // 5 minutes
-                )
-                ->onError(function (RetryContext $context, Throwable $exception) {
-                    if ($context->isLastAttempt()) {
-                        $this->logger->info("Connection to database failed, will not retry: {$exception->getMessage()}");
-                    } else {
-                        $this->logger->info("Connection to database failed, will retry: {$exception->getMessage()}");
-                    }
-                })
-            ;
-            $retry->execute(function () use ($connection) {
-                $this->logger->info('Connecting to database ...');
-                $connection->connect();
-                // explicitly auto commit
-                $connection->setAutoCommit(true);
-                $this->logger->info('Database connection established.');
-            });
-        }
-
-        return $connection;
     }
 }
